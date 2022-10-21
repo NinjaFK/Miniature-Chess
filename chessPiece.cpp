2742fad50a59f069734ec5e31ed79ac9d9d93cb8
Brian <nieh_brian@yahoo.com>
2022-10-21 11:49:22 -0700

    Added coments

--- chessPiece.cpp
+++ chessPiece.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include "chessPiece.h"
 
+// sets the color based into base class color
 chessPiece::chessPiece(bool color)
 {
 	this->color = color;
 }
-
+// returns the color of player
 bool chessPiece::getPlayerType() const
 {
 	return color;
 }
-
+// deallocates the chess pieces and sends a message
 chessPiece::~chessPiece()
 {
 	std::cout << "Piece removed from board.\n";
