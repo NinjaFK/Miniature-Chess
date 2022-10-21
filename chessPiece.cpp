999a8fa6fb966c1becf4d6b5b08ae0ba3f2b1bc6
Brian <nieh_brian@yahoo.com>
2022-10-20 17:30:39 -0700

    Added funtionality

--- chessPiece.cpp
+++ chessPiece.cpp
@@ -3,15 +3,15 @@
 
 chessPiece::chessPiece(bool color)
 {
-	
+	this->color = color;
 }
-	
+
 bool chessPiece::getPlayerType() const
 {
-	
+	return color;
 }
 
 chessPiece::~chessPiece()
 {
-	
+	std::cout << "Piece removed from board.\n";
 }
