ed152642392007123adb134494d8c031ca67f409
Brian <nieh_brian@yahoo.com>
2022-10-21 11:50:14 -0700

    Copy constuctor of chessPiece

--- queenType.cpp
+++ queenType.cpp
@@ -1,19 +1,15 @@
 #include "queenType.h"
 #include <iostream>
 
-queenType::queenType(bool color)
-{ 
-
-}
-	
-bool queenType::move(char startRow, int startCol, char endRow,
- int endCol, chessPiece*** board)
+queenType::queenType(bool color) : chessPiece(color)
 {
-	
+}
 
+bool queenType::move(char startRow, int startCol, char endRow,
+                     int endCol, chessPiece ***board)
+{
 }
 
 queenType::~queenType()
 {
-	
 }
