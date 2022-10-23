986bef960283eee98ebe3025cbea77bbe84e07da
Brian <nieh_brian@yahoo.com>
2022-10-23 02:16:56 -0700

    Added if to check if not null and same color

--- bishopType.cpp
+++ bishopType.cpp
@@ -1,20 +1,29 @@
 #include "bishopType.h"
 #include <cmath>
 #include <iostream>
-
-bishopType::bishopType(bool color)
+// calls parent class constuctor and sets color
+bishopType::bishopType(bool color) : chessPiece(color)
 {
-
 }
-	
-bool bishopType::move(char startRow, int startCol, char endRow, 
-	int endCol, chessPiece*** board)
+// returns a bool if allowed
+bool bishopType::move(char startRow, int startCol, char endRow,
+					  int endCol, chessPiece ***board)
 {
-	
+	// checks if end pos is not null or if has a piece of the same color
+	if (board[endRow - 'A'][endCol - 1] != nullptr)
+	{
+		if (board[endRow - 'A'][endCol - 1]->getPlayerType() ==
+			this->getPlayerType())
+		{
+			return false;
+		}
+	}
+	// check if move possible
+
+	// check if along the way is not null
 }
-
-
+// when piece is deleted cout that it was taken
 bishopType::~bishopType()
 {
-	
+	std::cout << "Bishop Taken.\n";
 }
