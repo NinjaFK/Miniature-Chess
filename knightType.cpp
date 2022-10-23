e0d4259c7e5b8aebb64417e2ea24f5e91f7bb81d
Brian <nieh_brian@yahoo.com>
2022-10-23 01:44:26 -0700

    Added if statements to move to check if possible

--- knightType.cpp
+++ knightType.cpp
@@ -1,19 +1,35 @@
 #include "knightType.h"
 #include <iostream>
-
-knightType::knightType(bool color)
-{ 
-
-}
-	
-bool knightType::move(char startRow, int startCol, char endRow,
- int endCol, chessPiece*** board)
+#include <cmath>
+// calls parent class constuctor and sets color
+knightType::knightType(bool color) : chessPiece(color)
 {
-	
 }
-
+// returns a bool if allowed
+bool knightType::move(char startRow, int startCol, char endRow,
+                      int endCol, chessPiece ***board)
+{
+    // checks if end pos is not null or if has a piece of the smae color
+    if (board[endRow - 'A'][endCol - 1] != nullptr)
+    {
+        if (board[endRow - 'A'][endCol - 1]->getPlayerType() ==
+            this->getPlayerType())
+        {
+            return false;
+        }
+    }
+    // check if move possible
+    if (!((abs((startRow - 'A') - (endRow - 'A')) == 2 &&
+           abs((startCol - 1) - (endCol - 1)) == 1) ||
+          (abs((startRow - 'A') - (endRow - 'A')) == 1 &&
+           abs((startCol - 1) - (endCol - 1)) == 2)))
+    {
+        return false;
+    }
+    return true;
+}
 
 knightType::~knightType()
 {
-	
+    std::cout << "Knight Taken.\n";
 }
