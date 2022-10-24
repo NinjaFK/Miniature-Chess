79f6bf0e1eb247e4228d896c855f32b617005915
Brian <nieh_brian@yahoo.com>
2022-10-24 03:21:21 -0700

    Test recomp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "knightType.h"
 #include "chessPiece.h"
+#include "bishopType.h"
 
 #define RESET "\033[0m"
 #define RED "\033[31m"
@@ -18,11 +19,33 @@ int main()
         for (int j = 0; j < 8; j++)
             board[i][j] = nullptr;
 
-    board[6][6] = new knightType(false);
-    board[5][4] = new knightType(true);
+    // board[6][6] = new knightType(false);
+    // board[5][4] = new knightType(true);
+    board[4][4] = new bishopType(false);
+    board[3][5] = new bishopType(false);
 
+    // knight moves
     // std::cout << board[6][6]->move('G', 7, 'E', 6, board) << '\n';
     // std::cout << board[6][6]->move('G', 7, 'F', 5, board) << '\n';
-    std::cout << board[6][6]->move('G', 7, 'E', 6, board) << '\n';
+    // std::cout << board[6][6]->move('G', 7, 'E', 6, board) << '\n';
+
+    // bishop
+    /* std::cout << "line 37\n";
+        std::cout << startR << '\n'
+                  << startC << '\n'
+                  << endR << '\n'
+                  << endC << '\n'
+                  << difR << '\n'
+                  << difc << '\n'
+                  << '\n'; */
+    /* std::cout
+            << (difR) / abs(difR) << '\n'
+            << (difc) / abs(difc) << '\n'
+            << '\n'
+            << startR + (-i * ((difR) / abs(difR))) << '\n'
+            << startC + (-i * ((difc) / abs(difc))) << '\n'; */
+
+    std::cout << board[4][4]->move('E', 5, 'C', 7, board) << '\n';
+
     return 0;
 }
