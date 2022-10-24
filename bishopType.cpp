cdeba3508e2d85520fe522a56f9320a6abe050de
Brian <nieh_brian@yahoo.com>
2022-10-24 03:21:09 -0700

    Changed check if conditions and refactored

--- bishopType.cpp
+++ bishopType.cpp
@@ -13,6 +13,8 @@ bool bishopType::move(char startRow, int startCol, char endRow,
 	int endC = endCol - 1;
 	int startR = startRow - 'A';
 	int startC = startCol - 1;
+	int difR = startR - endR;
+	int difc = startC - endC;
 	// checks if end pos is not null or if has a piece of the same color
 	if (board[endR][endC] != nullptr)
 	{
@@ -23,19 +25,23 @@ bool bishopType::move(char startRow, int startCol, char endRow,
 		}
 	}
 	// check if move possible
-	if (abs((startR - endR) / (startC - endC)) != 1)
+	if (abs(difR) != abs(difc))
 	{
 		return false;
 	}
 	// check if along the way is not null
-	for (int i = 0; i < abs(startR - endR) - 1; i++)
+	for (int i = 1; i < abs(difR); i++)
 	{
-		if (board[i * (-1 * (startR - endR))]
-				 [i * (-1 * (startC - endC))] != nullptr)
+		if (board[startR + (-i * ((difR) / abs(difR)))]
+				 [startC + (-i * ((difc) / abs(difc)))] != nullptr)
 		{
 			return false;
 		}
 	}
+
+	std::cout << "line 49\n";
+
+	return true;
 }
 // when piece is deleted cout that it was taken
 bishopType::~bishopType()
