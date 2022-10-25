6dc714f872b36b478866628a763d7f1b629cc5d7
Brian <nieh_brian@yahoo.com>
2022-10-25 14:32:45 -0700

    Test recomp

--- test.cpp
+++ test.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 //#include "knightType.h"
 #include "chessPiece.h"
-#include "rookType.h"
+//#include "rookType.h"
 //#include "bishopType.h"
+#include "queenType.h"
 
 #define RESET "\033[0m"
 #define RED "\033[31m"
@@ -24,9 +25,13 @@ int main()
     // board[5][4] = new knightType(true);
     // board[4][4] = new bishopType(false);
     // board[3][5] = new bishopType(false);
-    board[6][6] = new rookType(false);
+    // board[6][6] = new rookType(false);
     // board[5][6] = new rookType(false);
-    board[6][3] = new rookType(false);
+    // board[6][3] = new rookType(false);
+    board[6][6] = new queenType(false);
+    // board[5][6] = new queenType(false);
+    // board[6][3] = new queenType(false);
+    // board[2][2] = new queenType(false);
 
     // knight moves
     // std::cout << board[6][6]->move('G', 7, 'E', 6, board) << '\n';
@@ -51,7 +56,7 @@ int main()
     // std::cout << board[4][4]->move('E', 5, 'C', 7, board) << '\n';
 
     // rook
-    std::cout << board[6][6]->move('G', 7, 'F', 7, board) << '\n';
+    /* std::cout << board[6][6]->move('G', 7, 'F', 7, board) << '\n';
     std::cout << board[6][6]->move('G', 7, 'G', 8, board) << '\n';
     std::cout << board[6][6]->move('G', 7, 'H', 7, board) << '\n';
     std::cout << board[6][6]->move('G', 7, 'G', 6, board) << '\n';
@@ -61,7 +66,37 @@ int main()
     std::cout << board[6][6]->move('G', 7, 'A', 7, board) << '\n';
     std::cout << board[6][6]->move('G', 7, 'G', 1, board) << '\n';
     std::cout << board[6][6]->move('G', 7, 'C', 3, board) << '\n';
-    std::cout << board[6][6]->move('G', 7, 'E', 4, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'E', 4, board) << '\n'; */
+
+    // queen
+    // rook like moves
+    /* std::cout << board[6][6]->move('G', 7, 'F', 7, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'G', 8, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'H', 7, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'G', 6, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'F', 8, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'C', 7, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'E', 7, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'A', 7, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'G', 1, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'C', 3, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'E', 4, board) << '\n'; */
+    //  bishoop like moves
+    /* std::cout << board[6][6]->move('G', 7, 'F', 8, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'H', 8, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'H', 6, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'F', 6, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'D', 4, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'A', 1, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'B', 2, board) << '\n'; */
+    // random tests should be all zeros
+    /* std::cout << board[6][6]->move('G', 7, 'E', 6, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'H', 5, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'H', 4, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'H', 1, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'C', 4, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'B', 5, board) << '\n';
+    std::cout << board[6][6]->move('G', 7, 'C', 8, board) << '\n'; */
 
     return 0;
 }
