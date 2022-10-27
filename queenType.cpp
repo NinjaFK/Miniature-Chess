eb755ecb7453ca5939cd02525bee7264f4b65479
Brian <nieh_brian@yahoo.com>
2022-10-26 20:29:08 -0700

    Header comment and more comments

--- queenType.cpp
+++ queenType.cpp
@@ -1,13 +1,16 @@
+/*
+    Name: Brian Nieh, 5008139573, 1005, 5
+    Description: Miniature Chess
+    Input: Sequence of moves
+    Output: Game of chess
+*/
 #include "queenType.h"
 #include <iostream>
 // calls parent class constuctor and sets color
-queenType::queenType(bool color) : chessPiece(color)
-{
-}
+queenType::queenType(bool color) : chessPiece(color){}
 // returns a bool if allowed
 bool queenType::move(char startRow, int startCol, char endRow,
-                     int endCol, chessPiece ***board)
-{
+                     int endCol, chessPiece ***board){
     int endR = endRow - 'A';
     int endC = endCol - 1;
     int startR = startRow - 'A';
@@ -15,8 +18,7 @@ bool queenType::move(char startRow, int startCol, char endRow,
     int difR = startR - endR;
     int difc = startC - endC;
     // checks if end pos is not null or if has a piece of the same color
-    if (board[endR][endC] != nullptr)
-    {
+    if (board[endR][endC] != nullptr){
         if (board[endR][endC]->getPlayerType() ==
             this->getPlayerType())
         {
@@ -24,31 +26,28 @@ bool queenType::move(char startRow, int startCol, char endRow,
         }
     }
     // check if move possible
-    if ((startR != endR && startC != endC) && (abs(difR) != abs(difc)))
-    {
+    if ((startR != endR && startC != endC) && (abs(difR) != abs(difc))){
         return false;
     }
     // check if along the way is not null
-    for (int i = 1; i < abs(difR) || i < abs(difc); i++)
-    {
-        if (startR == endR)
-        {
+    for (int i = 1; i < abs(difR) || i < abs(difc); i++){
+        // if its on the same col check rook like
+        if (startR == endR){
             if (board[startR]
                      [startC + (-i * ((difc) / abs(difc)))] != nullptr)
             {
                 return false;
             }
         }
-        else if (startC == endC)
-        {
+        else if (startC == endC){
             if (board[startR + (-i * ((difR) / abs(difR)))]
                      [startC] != nullptr)
             {
                 return false;
             }
         }
-        else
-        {
+        // if on diagnal check bishop like
+        else{
             if (board[startR + (-i * ((difR) / abs(difR)))]
                      [startC + (-i * ((difc) / abs(difc)))] != nullptr)
             {
@@ -59,7 +58,6 @@ bool queenType::move(char startRow, int startCol, char endRow,
     return true;
 }
 // when piece is deleted cout that it was taken
-queenType::~queenType()
-{
+queenType::~queenType(){
     std::cout << "Queen Taken.\n";
 }
